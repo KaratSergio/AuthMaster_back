e46ed3cc74581a8e25c291522e6d6c8d104e06dd
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-05-25 12:50:28 +0300

    add user-controller + swagger start set

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('AuthMaster')
+    .setDescription('REST API Documentation auth service')
+    .setVersion('1.0.0')
+    .addTag('access control')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: createUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+  @Get()
+  getAll() {
+    return this.usersService.getAllUser();
+  }
+}
